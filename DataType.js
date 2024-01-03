502f0eabd09fd8c32a088428fa45a4e0c385323b
ANIKET_PANSE <84241376+Aniket-Dilip-Panse@users.noreply.github.com>
2024-01-03 19:32:38 +0530

    Add files via upload

---
+++ DataType.js
@@ -0,0 +1,57 @@
+// There are total 7 types of datatype in javascript
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+// nn ss bb u
+
+// number
+let number = 10;
+console.log(number);
+
+// string
+let name = "aniket";
+console.log(name);  
+
+// boolean
+let isTrue = true;
+console.log(isTrue);
+
+// undefined
+let undef = undefined;
+console.log(undef);
+
+// null
+let nul = null;
+console.log(nul);
+
+// symbol
+let sym = Symbol();
+console.log(sym);
+
+// bigint
+let big = 100n;
+console.log(big);
+
+// object
+let obj = {
+    name: "aniket",
+    age: 24
+}
+console.log(obj);
+
+// array
+let arr = [1, 2, 3, 4, 5];
+console.log(arr);
+
+let wether = false;
+if (wether){
+    console.log("Grab your umbrella");
+}else {
+    console.log("wear your sunglasses");
+}
+
+console.log(Math.floor(Math.random()))
---
+++ function.js
@@ -0,0 +1,24 @@
+// function in javascript is a block of code which take some inputs and return some specific output.
+function sum(a,b,name) {
+    console.log('namaskar ' + name);
+    return a+b
+}
+console.log(sum(2, 4, 'aniket'));
+
+const addition = (firstname,lastname) => {
+    console.log(firstname);
+    console.log(`myself ${firstname} ${lastname} new javascript programmer`);
+}
+console.log(addition("aniket","panse"))
+
+const divide = (a, b) => a / b;
+console.log(divide(10, 0));
+
+// arrow function with explicit return
+const sumArrow = (number, number2) => {
+    return number+number2
+}
+console.log(sumArrow(10,20));
+
+const mulArrow2 = (digit, digit2) => digit * digit2
+console.log(mulArrow2(10,20));
