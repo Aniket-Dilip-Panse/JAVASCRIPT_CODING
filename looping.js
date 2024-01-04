6c62084fb0ef163256d5e4f648f7bd37e1f78d18
ANIKET_PANSE <84241376+Aniket-Dilip-Panse@users.noreply.github.com>
2024-01-04 16:50:18 +0530

    loops in javascript

--- looping.js
+++ looping.js
@@ -5,3 +5,14 @@ let number = 5;
 for (let i = 1; i <= 10; i++) {
     console.log(`${number} * ${i} = ${number * i}`);
 }
+
+const fruits = ["apple", "banana", "chikoo", "dragon"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`${fruits[i]} is in index ${i}`);
+}
+
+const number_array = [];
+for (let j=0; j<=10; j+=2){
+    number_array.push(j);
+}
+console.log(number_array);
