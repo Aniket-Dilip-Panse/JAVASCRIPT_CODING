63b92b747cd904643260e10f59ecfc04b44b39d1
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-01-18 16:27:03 +0530

    mathModule.js file changes done to all method

--- mathModule.js
+++ mathModule.js
@@ -1,2 +1,35 @@
 // Math = built-in object that provides           collection of properties and method 
-Math.PI
+Math.PI 
+console.log(Math.PI)
+z = Math.round(10.5)
+console.log(z)
+
+a = Math.floor(10.5)
+console.log(a)
+
+b = Math.ceil(10.5)
+console.log(b)
+
+c = Math.pow(2,3)
+console.log(c)
+
+d = Math.sqrt(16)
+console.log(d)
+
+e = Math.abs(-10)
+console.log(e)
+
+f = Math.random()
+console.log(`The random number is ${f}*3`)
+
+g = Math.min(1,2,3,4,5,6,7,8,9,10)
+console.log(`The min number is ${g}`)
+
+h = Math.max(1,2,3,4,5,6,7,8,9,10)
+console.log(`The max number is ${h}`)
+
+i = Math.trunc(10.5)
+console.log(i)
+
+J = Math.random().random
+console.log(J)
