64b0c1c18443de850b11fa5b7d782693bc26d5ba
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-17 16:52:46 +0530

    tutorial 21 code added

---
+++ JavascriptByCodeWithHarry/tut21_practiceSet5.js
@@ -0,0 +1,18 @@
+// practice question based on array=========
+// Q1) create an array of numbers and take input from the user to add numbers to the array.
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+const readline2 = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const arr = [];
+
+const size = readline.question("enter the size of array: ", size => {
+    
+})
