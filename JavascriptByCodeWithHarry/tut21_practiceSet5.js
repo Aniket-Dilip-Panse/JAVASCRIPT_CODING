97c50edaa99bb77a1f49fcd2c2dbd9e61ff3700b
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-02-18 22:33:18 +0530

    new added

--- JavascriptByCodeWithHarry/tut21_practiceSet5.js
+++ JavascriptByCodeWithHarry/tut21_practiceSet5.js
@@ -1,18 +1,24 @@
 // practice question based on array=========
 // Q1) create an array of numbers and take input from the user to add numbers to the array.
-const readline = require("readline").createInterface({
+var readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
+var arr = [];
 
-const readline2 = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-const arr = [];
-
-const size = readline.question("enter the size of array: ", size => {  
+var size = readline.question("enter the size of array: ", size => {  
+    console.log(size);
     readline.close();
 })
+
+var readline2 = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let data = readline2.question("enter the value: ", value => {
+    for (let i = 0; i < size; i++) {
+        arr.push(value);
+    }
+});
