bd7b018eb99b18f7929b57ed2949ffda3b03ecc6
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-06 10:43:22 +0530

    new file arrow_function created and updated

---
+++ javascript/arrow_function.js
@@ -0,0 +1,17 @@
+const obj = {
+    username: 'aniket',
+    age: 23,
+    welcomemessage: function () {
+        console.log(`namaskar ${this.username} `);
+    },
+}
+obj.welcomemessage();
+
+// but if we change the username then the refrence also get change.
+
+obj.username = 'andya';
+obj.welcomemessage();
+
+console.log(obj);
+
+
