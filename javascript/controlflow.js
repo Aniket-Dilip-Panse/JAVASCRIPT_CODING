20da43c6d4159f14ce164d274e21babad1d762e9
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-08 23:35:37 +0530

    new commit

--- javascript/controlflow.js
+++ javascript/controlflow.js
@@ -1,1 +1,14 @@
 // control flow statement
+
+// if statement
+// if else statement
+// switch statement
+// ternary operator
+// for loop
+// for in loop
+// for of loop
+// while loop
+// do while loop
+// break statement
+// continue statement
+
