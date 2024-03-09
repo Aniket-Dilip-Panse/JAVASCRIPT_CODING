518ed6626465e99756163305b6af0e429562901c
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-09 23:42:08 +0530

    new changes

--- javascript/controlflow.js
+++ javascript/controlflow.js
@@ -12,3 +12,18 @@
 // break statement
 // continue statement
 
+if (true) {
+    let a = 10;
+    console.log(a);
+    var a = 20;
+    console.log(a);
+    const b = 10;
+    console.log(b);
+} else {
+    let a = 20;
+    console.log(a);
+    var a = 30;
+    console.log(a);
+    const b = 40;
+    console.log(b);
+}
