c0e80fb588bc1b4544f90ad5263e252c9715da2b
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-12 00:06:14 +0530

    adding functionality to button

---
+++ javascript/DOM_MANIPULATION/index.js
@@ -0,0 +1,19 @@
+function onclickbtn() {
+    document.getElementById("btn").style.backgroundColor = "red"
+}
+
+function doubleclick() {
+    document.getElementById("btn").style.backgroundColor = "blue"
+}
+
+function onmouseout() {
+    document.getElementById("btn").style.backgroundColor = "green"
+}
+
+function onmousemove() {
+    document.getElementById("btn").style.backgroundColor = "yellow"
+}
+
+function onmouseover() {
+    document.getElementById("btn").style.backgroundColor = "purple"
+}
