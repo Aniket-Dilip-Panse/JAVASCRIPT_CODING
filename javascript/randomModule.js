d9b5d62042e011c0e5e51d5a47fec5c0a0ed1a00
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-13 23:58:03 +0530

    random file upadated

--- javascript/randomModule.js
+++ javascript/randomModule.js
@@ -1,2 +1,9 @@
 let random = Math.random()  // random number between 0 and 1
 console.log(random)
+
+let random2 = Math.random() * 100
+console.log(random2)
+
+let random3 = Math.ceil(Math.random() * 100)
+console.log(random3)
+
