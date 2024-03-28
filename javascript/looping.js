3b0fbfdbe30091e44c48db30d064eaa94f16ae95
ANIKET_PANSE <aniketpanse12@gmail.com>
2024-03-28 15:03:08 +0530

    new push added

--- javascript/looping.js
+++ javascript/looping.js
@@ -21,3 +21,9 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i of arr) {
     console.log(arr[i]);
 }
+
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
